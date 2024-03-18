68bc91f6c6a14a4f9ec1f36112e4b47fe9739cfb
ethxnwzng <ewong29@lion.lmu.edu>
2024-03-18 13:39:00 -0700

    Initial Commit

---
+++ FirebasedBackedMobileApp/FirebasedBackedMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebasedBackedMobileAppApp.swift
+//  FirebasedBackedMobileApp
+//
+//  Created by Ethan Wong on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebasedBackedMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
